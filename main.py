e2bf912b96cbb7126cbf0365d622462ba799cef1
smendoza07 <mendozasaul07@gmail.com>
2024-03-15 13:33:20 -0700

    code will print out how many times a letter is found

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def main():
     file_contents = read_file()
     lower_cased = to_lower(file_contents)
     chars_dict = unique_letter_count(lower_cased)
-    chars_list = []
     chars_list = to_list(chars_dict)
     word_count = count_words(file_contents)
-    to_list(chars_dict)
-    print(chars_list)
+    for item in chars_list:
+        if item["char"].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
 
 def read_file():
     with open("books/frankenstein.txt") as f:
@@ -30,14 +30,13 @@ def unique_letter_count(lower_cased):
 
 def to_list(chars_dict):
     temp_list = []
-    for key, val in chars_dict.items():
-        if not isinstance(val, list):
-            val = [val]  # Convert non-list values to a list
-        temp_list.append([key] + val)
+    for ch in chars_dict:
+        temp_list.append({"char": ch, "num": chars_dict[ch]})
+    temp_list.sort(reverse=True, key=sort_on)
     return temp_list
 
 
-# def sort_on(dict):
-#     return dic[]
+def sort_on(d):
+    return d["num"]
 
 main()
